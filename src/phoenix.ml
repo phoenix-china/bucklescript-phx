0822418fd1dbe5ca1f3e5699622973e96cd58bc3
jackalcooper <jackalcooper@gmail.com>
2017-07-03 20:56:17 +0800

    Bump version. Make code even more succinct

--- src/phoenix.ml
+++ src/phoenix.ml
@@ -8,25 +8,25 @@ open Abstract
 
 external initSocket : string -> ?opts:('opts Js.t) -> unit -> Socket.t =  "Socket" [@@bs.new] [@@bs.module "phoenix"]
 let initSocket ?opts:(opts: ('opts Js.t) option) path =
-  initSocket path ?opts:opts ()
+  initSocket path ?opts ()
 
 external initChannel : Socket.t -> string -> ?chanParams:('chanParams Js.t) -> unit -> Channel.t  = "channel" [@@bs.send]
-let initChannel topic ?chanParams:(chanParams: ('chanParams Js.t) option) socket = initChannel socket topic ?chanParams:chanParams ()
+let initChannel topic ?chanParams:(chanParams: ('chanParams Js.t) option) socket = initChannel socket topic ?chanParams ()
 
 (*Channel functions*)
 
 external joinChannel : Channel.t -> ?timeout:float -> unit -> Push.t = "join" [@@bs.send]
 let joinChannel ?timeout:(timeout: float option) channel =
-  joinChannel channel ?timeout:timeout ()
+  joinChannel channel ?timeout ()
 
 external leaveChannel : Channel.t -> ?timeout:float -> unit -> Push.t = "leave" [@@bs.send]
 let leaveChannel ?timeout:(timeout: float option) channel =
-  leaveChannel channel ?timeout:timeout ()
+  leaveChannel channel ?timeout ()
 
 
 external push : Channel.t -> string -> payloadObject -> ?timeout:float -> unit -> Push.t = "push" [@@bs.send]
 let push event object_ ?timeout:(timeout: float option) channel =
-   push channel event object_ ?timeout:timeout ()
+   push channel event object_ ?timeout ()
 
 let putOn event (f:(any -> void )) channel = let _ = channel##on event f in channel
 
@@ -34,10 +34,10 @@ let putOn event (f:(any -> void )) channel = let _ = channel##on event f in chan
 
 external disconnectSocket : Socket.t -> ?callback:function_ -> ?code:string -> ?reason:('reason Js.t) -> unit -> void = "disconnect"[@@bs.send]
 let disconnectSocket ?callback:(callback: function_ option) ?code:(code: string option) ?reason:(reason: ('reason Js.t) option) socket = let _ =
-  disconnectSocket ?callback:callback ?code:code ?reason:reason socket () in socket
+  disconnectSocket ?callback ?code ?reason socket () in socket
 
 external connectSocket : Socket.t -> ?params:('params Js.t) -> unit -> void = "connect" [@@bs.send]
-let connectSocket ?params:(params: ('params Js.t) option) socket = let _ = connectSocket ?params:params socket () in socket
+let connectSocket ?params:(params: ('params Js.t) option) socket = let _ = connectSocket ?params socket () in socket
 
 external removeChannel : Socket.t -> Channel.t -> void = "remove" [@@bs.send]
 let removeChannel channel socket = removeChannel socket channel
--- src/phx_presence.ml
+++ src/phx_presence.ml
@@ -8,13 +8,13 @@ type diff = < joins : state; leaves : state > Js.t
 type onJoinOrLeave = (?key:string -> ?currentPresence:state -> ?newPresence:state -> unit -> void)
 
 external syncState : state -> state -> ?onJoin:onJoinOrLeave -> ?onLeave:onJoinOrLeave -> unit -> state = "syncState" [@@bs.scope "Presence"] [@@bs.module "phoenix"]
-let syncState ?onJoin:(on_join: onJoinOrLeave option) ?onLeave:(on_leave: onJoinOrLeave option) currentState newState =
-  syncState currentState newState ?onJoin:on_join ?onLeave:on_leave ()
+let syncState ?onJoin:(onJoin: onJoinOrLeave option) ?onLeave:(onLeave: onJoinOrLeave option) currentState newState =
+  syncState currentState newState ?onJoin ?onLeave ()
 
 
 external syncDiff : state -> diff -> ?onJoin:onJoinOrLeave -> ?onLeave:onJoinOrLeave -> unit -> state = "syncDiff" [@@bs.scope "Presence"] [@@bs.module "phoenix"]
-let syncDiff ?onJoin:(on_join: onJoinOrLeave option) ?onLeave:(on_leave: onJoinOrLeave option) currentState diff =
-  syncDiff currentState diff ?onJoin:on_join ?onLeave:on_leave ()
+let syncDiff ?onJoin:(onJoin: onJoinOrLeave option) ?onLeave:(onLeave: onJoinOrLeave option) currentState diff =
+  syncDiff currentState diff ?onJoin ?onLeave ()
 
 
 let empty = Js.Dict.empty ()
